1a0147871b18c51246b967607cdfefe23192d0b5
Andhitia Rama <andhitia.r@gmail.com>
2018-06-17 17:52:19 +0700

    Adjusting to installation script

--- odoo-hw-box-config.sh
+++ odoo-hw-box-config.sh
@@ -2,7 +2,7 @@
 
 # Get variable from config file
 
-source config
+source /etc/odoo-hw-box-config
 ODOO_PIDFILE=${odoo_pid_file}
 ODOO_CONFIG_FILE=${odoo_configuration_file}
 ODOO_DAEMON_FILE=${odoo_daemon_file}
